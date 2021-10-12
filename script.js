f36e9f2033ea938a1ed0b11a52d6b39a05c4bda4
Ivon Badra <ibadra.c@gmail.com>
2021-10-12 00:00:05 -0400

    Form styling

--- script.js
+++ script.js
@@ -8,12 +8,26 @@ const formText = document.getElementById("message");
 
 //STYLES
 formName.style.fontFamily = "Varela Round";
+formName.style.color = "#9d84b7";
+formEmail.style.fontFamily = "Varela Round";
+formEmail.style.color = "#9d84b7";
+formText.style.fontFamily = "Varela Round";
+formText.style.color = "#9d84b7";
 
 //EVENT LISTENERS
 submitBtn.addEventListener("click", function () {
   setTimeout(openModal, 3000);
 });
 closeBtn.addEventListener("click", closeModal);
+formName.addEventListener("mousedown", function () {
+  formName.style.outlineColor = "#eb92be";
+});
+formEmail.addEventListener("mousedown", function () {
+  formEmail.style.outlineColor = "#eb92be";
+});
+formText.addEventListener("mousedown", function () {
+  formText.style.outlineColor = "#eb92be";
+});
 //FUNCTIONS
 function openModal() {
   modal.style.visibility = "visible";
